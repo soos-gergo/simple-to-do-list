511b9d6e98c1ced6b6e74fb71e974210455a531f
Gergő Soós <gergosoos4@gmail.com>
2023-03-23 09:24:57 +0100

    I fixed 'Add' button function.
    I fixed 'Add' button function. I replace from 'araiValue.length' to 'value.length'. It checks the text field has content.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').ariaValueMax.length == 0) {
+    if(document.querySelector('#newtask input').value.length == 0) {
         alert("Please enter a task!");
     }
     else {
